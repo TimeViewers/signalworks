a361097505c0f5ce1f87d310c1604b9d9b988760
Ogi Moore <ognyan.moore@jacobs.ucsd.edu>
2018-07-28 21:07:14 -0700

    migrated content from timeview here, broke up processors

---
+++ signalworks/processors/activity_detector.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from typing import NamedTuple, Tuple
+
+import numpy as np
+
+from processing import Processor
+from .tracking import Wave, TimeValue, Partition
+from signalworks import dsp, viterbi
+
+
+class ActivityDetector(Processor):
+    name = "Activity Detector"
+    acquire = NamedTuple("acquire", [("wave", Wave)])
+
+    def __init__(self):
+        super().__init__()
+        self.parameters = {
+            "threshold": -30.0,
+            "smooth": 1.0,
+            "frame_size": 0.020,  # in seconds
+            "frame_rate": 0.01,
+        }
+
+    def process(self, progressTracker=None, **kwargs) -> Tuple[Partition, TimeValue]:
+        if progressTracker is not None:
+            self.progressTracker = progressTracker
+        wav = self.data.wave
+        wav = wav.convert_dtype(np.float64)
+        self.progressTracker.update(10)
+        M, time, frequency = dsp.spectrogram(
+            wav, self.parameters["frame_size"], self.parameters["frame_rate"]
+        )
+        self.progressTracker.update(20)
+        # Emax = np.atleast_2d(np.max(M, axis=1)).T
+        Emax = 20 * np.log10(np.mean((10 ** (M / 10)), axis=1) ** 0.5)
+        P = np.empty((len(Emax), 2))
+        P[:, 0] = 1 / (1 + np.exp(Emax - self.parameters["threshold"]))
+        P[:, 1] = 1 - P[:, 0]  # complement
+        self.progressTracker.update(30)
+        seq, _ = viterbi.search_smooth(P, self.parameters["smooth"])
+        self.progressTracker.update(90)
+        tmv = TimeValue(
+            time,
+            seq,
+            wav.fs,
+            wav.duration,
+            wav.path.with_name(wav.path.stem + "-act").with_suffix(
+                TimeValue.default_suffix
+            ),
+        )
+        par = Partition.from_TimeValue(tmv)
+        par.value = np.char.mod("%d", par.value)
+        emax = TimeValue(
+            time,
+            Emax,
+            wav.fs,
+            wav.duration,
+            wav.path.with_name(wav.path.stem + "-emax").with_suffix(
+                TimeValue.default_suffix
+            ),
+        )
+        emax.min = Emax.min()
+        emax.max = Emax.max()
+        emax.unit = "dB"
+        emax.label = "maximum frequency magnitude"
+        return par, emax
---
+++ signalworks/processors/processing.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+import logging
+from abc import ABCMeta, abstractmethod
+from typing import Tuple, Dict, Union, Callable, NamedTuple, Optional, Any
+
+import numpy as np
+
+from signalworks.tracking import Wave, Partition, TimeValue
+
+Tracks = Union[Wave, TimeValue, Partition]
+# type alias
+
+
+class Data(NamedTuple):
+    wave: Optional[Wave]
+    active: Optional[Partition]
+
+
+class InvalidDataError(Exception):
+    pass
+
+
+class InvalidParameterError(Exception):
+    pass
+
+
+class ProcessError(Exception):
+    pass
+
+
+class DefaultProgressTracker:
+    def update(self, value: int):
+        print(f"{value}%", end="...", flush=True)
+
+
+class Processor(metaclass=ABCMeta):
+    name = "Processor"
+    acquire = NamedTuple("acquire", [("wave", Wave), ("active", Partition)])
+
+    def __init__(self):
+        self.data = Data(wave=None, active=None)
+        self.parameters: Dict[str, Any] = {}  # default parameters
+        self.progressTracker = DefaultProgressTracker()
+
+    def set_data(self, data: Dict[str, Tracks]) -> None:
+        wav = data.get("wave")
+        if wav is not None and not isinstance(wav, Wave):
+            raise InvalidDataError
+
+        active = data.get("active")
+        if active is not None and not isinstance(wav, Partition):
+            raise InvalidDataError
+
+        self.data = Data(wave=wav, active=active)
+
+    def get_parameters(self) -> Dict[str, str]:
+        # default parameters can be modified here based on the data
+        return {k: str(v) for k, v in self.parameters.items()}
+
+    def set_parameters(self, parameters: Dict[str, str]) -> None:
+        if __debug__:
+            for name, value in parameters.items():
+                logging.debug(f"Received parameter {name} of value {value}")
+        try:
+            for key in parameters.keys():
+                if type(self.parameters[key]) == np.ndarray:
+                    self.parameters[key] = np.fromstring(
+                        parameters[key].rstrip(")]").lstrip("[("), sep=" "
+                    )
+                else:
+                    self.parameters[key] = type(self.parameters[key])(parameters[key])
+        except Exception as e:
+            raise InvalidParameterError(e)
+        # additional parameter checking can be performed here
+
+    @abstractmethod
+    def process(self, progressTracker=None) -> Tuple[Tracks, ...]:
+        pass
+
+    def del_data(self):
+        self.data = Data(wave=None, active=None)
+
+
+def get_processor_classes() -> Dict[str, Callable[..., Processor]]:
+    def all_subclasses(c):
+        return c.__subclasses__() + [
+            a for b in c.__subclasses__() for a in all_subclasses(b)
+        ]
+
+    return {obj.name: obj for obj in all_subclasses(Processor)}
