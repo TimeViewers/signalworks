88dac6c2fba9c5d92eac36dbf9f9b029bd6202d0
tuanad121 <tuanad121@gmail.com>
2019-04-18 15:26:47 -0700

    Update __init__.py
    strange error when importing MultiTrack before others

--- signalworks/tracking/__init__.py
+++ signalworks/tracking/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
+from .metatrack import MetaTrack  # noqa
 from .error import LabreadError, MultiChannelError  # noqa
 from .event import Event  # noqa
 from .label import Label  # noqa
-from .metatrack import MetaTrack  # noqa
-from .multitrack import MultiTrack  # noqa
 from .partition import Partition  # noqa
 from .timevalue import TimeValue  # noqa
 from .tracking import Track  # noqa: F401
 from .value import Value  # noqa
 from .wave import Wave  # noqa
+from .multitrack import MultiTrack  # noqa
