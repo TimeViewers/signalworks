4c171c1b858565bdfc4e36a19a22631e375e95dd
tuanad121 <tuanad121@gmail.com>
2019-04-23 10:17:31 -0700

    Integrate benchmark (#7)
    * Update __init__.py
    
    strange error when importing MultiTrack before others
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Delete azure-pipelines.yml
    
    * add pytest-benchmark for profiling
    
    * fix packages=find_packages()

--- tests/test_tracking.py
+++ tests/test_tracking.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-import pytest  # noqa: F401
-
 import numpy as np
-
-from signalworks.tracking.wave import Wave
+import pytest  # noqa: F401
 from signalworks import dsp
+from signalworks.tracking.wave import Wave
 
 
-def test_spectrogram(speech_track):
+def test_spectrogram(speech_track, benchmark):
     frame_size = 0.030
     half = frame_size * speech_track.fs // 2
     centers = np.round(np.linspace(half, speech_track.duration - half, 1000)).astype(
@@ -16,7 +14,12 @@ def test_spectrogram(speech_track):
 
     NFFT = 2 ** dsp.nextpow2(512 * 2)
     assert isinstance(speech_track, Wave)
-    X, f = dsp.spectrogram_centered(
-        speech_track, frame_size, centers, NFFT=NFFT, normalized=False
+    X, f = benchmark(
+        dsp.spectrogram_centered,
+        speech_track,
+        frame_size,
+        centers,
+        NFFT=NFFT,
+        normalized=False,
     )
     assert X.shape == (len(centers), NFFT // 2 + 1)
